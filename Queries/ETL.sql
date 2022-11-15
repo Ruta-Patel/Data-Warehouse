f11a1932d01f52c70e2ee87dd0c459d96ad20597
Ruta <99355446+Ruta-Patel@users.noreply.github.com>
2022-11-15 13:55:13 +0100

    Update ETL.sql

--- Queries/ETL.sql
+++ Queries/ETL.sql
@@ -1,7 +1,7 @@
-<!--ETL Process:-->
+ETL Process:
 
-To get the data together from all the dataset of years 2000 to 2016, the data is merged into single 
-master dataset and named as water_quality. For merging the data select and union query is used.
+/*To get the data together from all the dataset of years 2000 to 2016, the data is merged into single 
+master dataset and named as water_quality. For merging the data select and union query is used.*/
 
 INSERT INTO WATER_QUALITY
 SELECT * FROM "2000"
